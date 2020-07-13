8b054de990a3b3d265c2af5b6161a75d927df52e
nishantcrypto <singhnishant.nish@gamil.com>
2020-07-13 21:15:24 +0530

    pyaudio added and email codes have been changed

--- jarvis.py
+++ jarvis.py
@@ -1,4 +1,4 @@
-import pyttsx3 #pip install pyttsx3
+import pyttsx3 #pip install pyttsx3 it is text to speech library
 import speech_recognition as sr #pip install speechRecognition
 import datetime
 import wikipedia #pip install wikipedia
@@ -8,6 +8,7 @@ import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
+# print(voices[1].id)
 engine.setProperty('voice', voices[1].id)
 
 
@@ -27,7 +28,7 @@ def wishMe():
     else:
         speak("Good Evening!")  
 
-    speak("Hello Sir,I am Jarvis at your service. How may I help you")       
+    speak("Hello Sir, It's good that you are back , I am ready to serve. How may I help you")       
 
 def takeCommand():
     #It takes microphone input from the user and returns string output
@@ -49,61 +50,61 @@ def takeCommand():
         return "None"
     return query
 
-# def sendEmail(to, content):
-#     server = smtplib.SMTP('smtp.gmail.com', 587)
-#     server.ehlo()
-#     server.starttls()
-#     server.login('youremail@gmail.com', 'your-password')
-#     server.sendmail('youremail@gmail.com', to, content)
-#     server.close()
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('singhnishant.nish@gmail.com', 'your-password')
+    server.sendmail('singhnishant.nish@gmail.com', to, content)
+    server.close()
 
 if __name__ == "__main__":
     wishMe()
-    takeCommand()
-    # while True:
-    # # if 1:
-    #     query = takeCommand().lower()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
 
-    #     # Logic for executing tasks based on query
-    #     if 'wikipedia' in query:
-    #         speak('Searching Wikipedia...')
-    #         query = query.replace("wikipedia", "")
-    #         results = wikipedia.summary(query, sentences=2)
-    #         speak("According to Wikipedia")
-    #         print(results)
-    #         speak(results)
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
 
-    #     elif 'open youtube' in query:
-    #         webbrowser.open("youtube.com")
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
 
-    #     elif 'open google' in query:
-    #         webbrowser.open("google.com")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
 
-    #     elif 'open stackoverflow' in query:
-    #         webbrowser.open("stackoverflow.com")   
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
 
 
-    #     elif 'play music' in query:
-    #         music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
-    #         songs = os.listdir(music_dir)
-    #         print(songs)    
-    #         os.startfile(os.path.join(music_dir, songs[0]))
+        elif 'play music' in query:
+            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
 
-    #     elif 'the time' in query:
-    #         strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-    #         speak(f"Sir, the time is {strTime}")
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
 
-    #     elif 'open code' in query:
-    #         codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-    #         os.startfile(codePath)
+        elif 'open code' in query:
+            codePath = "C:\\Users\\shashank singh\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            
+            os.startfile(codePath)
 
-    #     elif 'email to nishant' in query:
-    #         try:
-    #             speak("What should I say?")
-    #             content = takeCommand()
-    #             to = "singhnishant.nish@gmail.com"    
-    #             sendEmail(to, content)
-    #             speak("Email has been sent!")
-    #         except Exception as e:
-    #             print(e)
-    #             speak("Sorry Sir. I am not able to send this email")    
+        elif 'email to harry' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "singhnishant.nish@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry Sir. I am not able to send this email")    
